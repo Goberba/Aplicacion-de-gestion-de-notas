dff02089398a07a51a1f19ad035445b8add4af0d
Alberto <abuenocr21dw@ikzubirimanteo.com>
2022-01-31 13:24:16 +0100

    nada

--- src/Ciclo.java
+++ src/Ciclo.java
@@ -5,7 +5,9 @@ public class Ciclo {
 	private int duracionHoras;
 	
 	public Ciclo(String i, String n, int horas) {
+		
 		this.identificadorGrupo=i;
+		System.out.println("Nada");
 		this.nomCiclo=n;
 		this.duracionHoras=horas;
 	}
