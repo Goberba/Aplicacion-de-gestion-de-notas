efc63bdb856548f49e9ae01307427679af9eccc8
Goberba <gbermejoba21dw@ikzubirimanteo.com>
2022-01-31 13:09:10 +0100

    Clase Ciclo

--- src/Ciclo.java
+++ src/Ciclo.java
@@ -9,30 +9,6 @@ public class Ciclo {
 		this.nomCiclo=n;
 		this.duracionHoras=horas;
 	}
-
-	public String getIdentificadorGrupo() {
-		return identificadorGrupo;
-	}
-
-	public void setIdentificadorGrupo(String identificadorGrupo) {
-		this.identificadorGrupo = identificadorGrupo;
-	}
-
-	public String getNomCiclo() {
-		return nomCiclo;
-	}
-
-	public void setNomCiclo(String nomCiclo) {
-		this.nomCiclo = nomCiclo;
-	}
-
-	public int getDuracionHoras() {
-		return duracionHoras;
-	}
-
-	public void setDuracionHoras(int duracionHoras) {
-		this.duracionHoras = duracionHoras;
-	}
 	
 	
 
