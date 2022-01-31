eb816f2e4dfbe73ca015769034e8b609579a98a2
Goberba <gbermejoba21dw@ikzubirimanteo.com>
2022-01-31 12:57:00 +0100

    Creación de la clase Ciclo y su constructor.

---
+++ src/Ciclo.java
@@ -0,0 +1,13 @@
+
+public class Ciclo {
+	private String identificadorGrupo;
+	private String nomCiclo;
+	private int duracionHoras;
+	
+	public Ciclo(String i, String n, int horas) {
+		this.identificadorGrupo=i;
+		this.nomCiclo=n;
+		this.duracionHoras=horas;
+	}
+
+}
