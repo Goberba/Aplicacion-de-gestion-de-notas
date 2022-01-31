f5c00ab3d675d8b9031ba0d6001332e9ce1238b5
Alberto <abuenocr21dw@ikzubirimanteo.com>
2022-01-31 13:30:39 +0100

    Merge branch 'master' of https://github.com/Goberba/Aplicacion-de-gestion-de-notas.git

---
+++ Aplicacion-de-gestion-de-notas/src/Ciclo.java
@@ -0,0 +1,15 @@
+
+public class Ciclo {
+	private String identificadorGrupo;
+	private String nomCiclo;
+	private int duracionHoras;
+	
+	public Ciclo(String i, String n, int horas) {
+	
+		this.identificadorGrupo=i;
+		System.out.println("Nada");
+		this.nomCiclo=n;
+		this.duracionHoras=horas;
+	}
+	
+}
