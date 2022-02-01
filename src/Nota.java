0dfc81220236487ccd7da40d8ec84eddfd1536f8
Alberto <abuenocr21dw@ikzubirimanteo.com>
2022-02-01 09:07:07 +0100

    Nueva "Nota"

--- src/Nota.java
+++ src/Nota.java
@@ -1,11 +1,11 @@
 
-public class Notas {
+public class Nota {
 	private int anio;
 	private int evaluacion;
 	private String modulo;
 	private double nota;
 	
-	public Notas(int a, int e, String m, double n) {
+	public Nota(int a, int e, String m, double n) {
 		this.anio=a;
 		this.evaluacion=e;
 		this.modulo=m;
