89f56e8e730db65c82b40d87b1c08d88877d804a
andoni <acarballogu21dw@ikzubirimanteo.com>
2022-02-01 08:17:01 +0100

    Alumno 0.1

---
+++ src/Alumno.java
@@ -0,0 +1,29 @@
+
+import java.util.Date;
+
+public class Alumno {
+    private String DNI;
+    private String nombre;
+    private Date fecha;
+    
+    public Alumno(String dni, String nom, Date fecha) {
+        this.DNI=dni;
+        this.nombre=nom;
+        this.fecha=fecha;
+    }
+    public Alumno getAlumno() {
+        
+    }
+    public void meterAlumno() {
+        
+    }
+    public void borrarAlumno() {
+        
+    }
+    public String toString() {
+        
+    }
+    
+    
+    
+}
