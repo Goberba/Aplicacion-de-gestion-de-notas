2ab7405e432723bf22ada8bc983b06f69d2944d4
Alberto <abuenocr21dw@ikzubirimanteo.com>
2022-02-01 08:33:07 +0100

    Clase Notas

---
+++ src/Notas.java
@@ -0,0 +1,16 @@
+
+public class Notas {
+	private int anio;
+	private int evaluacion;
+	private String modulo;
+	private double nota;
+	
+	public Notas(int a, int e, String m, double n) {
+		this.anio=a;
+		this.evaluacion=e;
+		this.modulo=m;
+		this.nota=n;
+	}
+	
+
+}
