ff10588599511ff2917669b51cc866b7180f1ca7
Goizeder <gbermejoba21dw@ikzubirimanteo.com>
2022-02-01 08:17:33 +0100

    Clase Ciclo

--- src/Ciclo.java
+++ src/Ciclo.java
@@ -7,7 +7,6 @@ public class Ciclo {
 	public Ciclo(String i, String n, int horas) {
 	
 		this.identificadorGrupo=i;
-		System.out.println("Nada");
 		this.nomCiclo=n;
 		this.duracionHoras=horas;
 	}
